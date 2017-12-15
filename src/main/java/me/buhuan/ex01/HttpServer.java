44b4a2194db222f1b0edc68711ec776405803815
黄步欢 <buhuanhuang@gmail.com>
2017-12-15 22:50:59 +0800

    初始化:tada:

---
+++ src/main/java/me/buhuan/ex01/HttpServer.java
@@ -0,0 +1,60 @@
+package me.buhuan.ex01;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.*;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/12/15
+ */
+public class HttpServer {
+    
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+    
+    private static final String SHUTDOWN_COMMAD = "/SHUTDOWN";
+    
+    private boolean shutdown = false;
+    
+    public static void main(String[] args) {
+        HttpServer httpServer = new HttpServer();
+        httpServer.await();
+    }
+    
+    public void await() {
+        ServerSocket serverSocket = null;
+        int port = 8080;
+        try {
+            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (!shutdown) {
+            Socket socket = null;
+            InputStream input = null;
+            OutputStream output = null;
+            try {
+                socket = serverSocket.accept();
+                input = socket.getInputStream();
+                output = socket.getOutputStream();
+                Request request = new Request(input);
+                request.parse();
+                Response response = new Response(output);
+                response.serRequest(request);
+                response.sendStaticResource();
+                socket.close();
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAD);
+            } catch (IOException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+    }
+    
+    
+}
