9d628af8733ef2deeb2bf30b981e29c9f06019a6
黄步欢 <buhuanhuang@gmail.com>
2017-12-15 23:36:00 +0800

    添加HttpServer1类

---
+++ src/main/java/me/buhuan/ex02/HttpServer1.java
@@ -0,0 +1,21 @@
+package me.buhuan.ex02;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/12/15
+ */
+public class HttpServer1 {
+    
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+    
+    private boolean shutdown = false;
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public void await() {
+        
+    }
+}
